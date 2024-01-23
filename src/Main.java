f0f944815b9f7d860f05d10b732cd0104e2c8688
Ekaterina <fhhs567@gmail.com>
2024-01-23 14:26:40 +1000

    tree

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.add(7);
+        tree.add(6);
+        tree.add(3);
+        tree.add(9);
+        tree.add(2);
+        tree.add(8);
+        tree.add(11);
+    }
+}
